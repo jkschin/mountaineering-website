27250baf8733c0fd07d2da47c2139838d2e92d27
Samuel Chin <samuelchin91@gmail.com>
2016-05-04 20:56:52 +0800

    Added get_first_image in post helpers using Nokogiri

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,11 @@ module PostsHelper
 	    render :text => 'Admins only :)'
 	  end
 	end
+
+	def get_first_image(content)
+		doc = Nokogiri::HTML( content )
+		img_srcs = doc.css('img').map{ |i| i['src'] }
+		return img_srcs[0]
+	end
+	
 end
