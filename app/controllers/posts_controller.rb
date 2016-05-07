74cfa5f559444f33a4ae1b3539dade73ae49f9f7
Samuel Chin <samuelchin91@gmail.com>
2016-05-07 18:09:48 +0800

    Reverse order of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   respond_to :html
 
   def index
-    @posts = Post.all
+    @posts = Post.all(:order => "created_at DESC")
     respond_with(@posts)
   end
 
